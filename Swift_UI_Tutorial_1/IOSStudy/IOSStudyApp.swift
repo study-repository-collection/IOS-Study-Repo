76b964c2da2f64100cf8454a76f6b295eabecb98
Park <pjh00098@gmail.com>
2022-02-10 21:53:32 +0900

    first commit

---
+++ Swift_UI_Tutorial_1/IOSStudy/IOSStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSStudyApp.swift
+//  IOSStudy
+//
+//  Created by 박종호 on 2022/02/10.
+//
+
+import SwiftUI
+
+@main
+struct IOSStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
